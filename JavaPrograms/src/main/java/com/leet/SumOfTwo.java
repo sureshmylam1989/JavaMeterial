34df2a38ca8e654816bd2d2727061f58ee791b1b
sureshkumarmylam <sureshkumarmylam@192.168.0.20>
2020-04-19 18:44:02 -0400

    updated

--- JavaPrograms/src/main/java/com/leet/SumOfTwo.java
+++ JavaPrograms/src/main/java/com/leet/SumOfTwo.java
@@ -25,26 +25,12 @@ public class SumOfTwo {
 		}
 		throw new IllegalArgumentException("No two sum solution");
 	}
-	
-//	public int[] twoSum(int[] nums, int target) {
-//		Map<Integer, Integer> map = new HashMap<>();
-//		for (int i = 0; i < nums.length; i++) {
-//			map.put(nums[i], i);
-//		}
-//		for (int i = 0; i < nums.length; i++) {
-//			int complement = target - nums[i];
-//			if (map.containsKey(complement) && map.get(complement) != i) {
-//				return(new int[] { i, map.get(complement) });
-//			}
-//		}
-//		throw new IllegalArgumentException("No two sum solution");
-//	}
 
 	public static void main(String[] args) {
 		int[] nums = { 2, 7, 3, 4, 5, 6, 7, 9 };
-		System.out.println(new SumOfTwo().twoSum(nums,11)[0]);
-		System.out.println(new SumOfTwo().twoSum(nums,11)[1]);
-		
+		System.out.println(new SumOfTwo().twoSum(nums, 11)[0]);
+		System.out.println(new SumOfTwo().twoSum(nums, 11)[1]);
+
 	}
 
 }
