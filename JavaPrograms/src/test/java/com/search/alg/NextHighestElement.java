6d1d6bd5a13a2d6febce532d78393adc0ce6f0c3
sureshkumarmylam <sureshkumarmylam@192.168.0.20>
2020-04-21 19:12:30 -0400

    UPDATED

--- JavaPrograms/src/test/java/com/search/alg/NextHighestElement.java
+++ JavaPrograms/src/test/java/com/search/alg/NextHighestElement.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class NextHighestElement {
 
 	public static void main(String[] args) {
-		int[] a = { 5, 3, 2, 10, 6, 8, 1, 4, 12, 7, 4 };
+		int[] a = { 3, 5, 1, 2, 16, 8, 6, 9, 16, 9, 7 };
 		nextHighest(a);
 		for (int b = 0; b < a.length; b++) {
 			System.out.println(a[b]);
@@ -14,7 +14,7 @@ public class NextHighestElement {
 
 	// replace each element with next highest element if any else leave the element
 	public static void nextHighest(int[] a) {
-		
+
 		// Stack ---> push if element is > than top else pop
 		Stack<Integer> st = new Stack<Integer>();
 
